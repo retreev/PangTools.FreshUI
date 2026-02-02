420fa240c66b4a618839aefd5c4d31b6d31b3b3e
Andreas Nedbal <github-bf215181b5140522137b3d4f6b73544a@desu.email>
2026-02-02 01:04:04 +0100

    Add StringExtensions to Renderer

---
+++ PangTools.FreshUI.Renderer/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace PangTools.FreshUI.Renderer;
+
+public static class StringExtensions
+{
+    public static bool EndsWith(this string value, params string[] values)
+    {
+        return values.Any(value.EndsWith);
+    }
+}
