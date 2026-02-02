e3c892bdee331358345503ece4342c448d7d97b3
Andreas Nedbal <github-bf215181b5140522137b3d4f6b73544a@desu.email>
2026-02-02 01:11:46 +0100

    Add color helper to transform Pangya color format

---
+++ PangTools.FreshUI.Renderer/Helpers/ColorHelper.cs
@@ -0,0 +1,16 @@
+﻿namespace PangTools.FreshUI.Renderer.Helpers;
+
+public static class ColorHelper
+{
+    public static string ARGBtoRBGA(string hexColor)
+    {
+        if (hexColor.StartsWith("0x"))
+        {
+            hexColor = hexColor.Substring(2);
+        }
+        
+        string alpha = hexColor.Substring(0, 2);
+
+        return hexColor.Substring(2) + alpha;
+    }
+}
